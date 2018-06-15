184050dceda539c135409322561b245f040addbd
Mathew Norman <spam@matnorman.com>
2018-06-16 09:56:00 +1000

    Added comments to results.php

--- example/results.php
+++ example/results.php
@@ -1,17 +1,23 @@
 <?php
+    // Specify the location of the word list file.
     $wordListFile = "./files/words.txt";
 
+    // Include the search.list.php file.
     require "../php/search.list.php";
     $ListSearch = new ListSearch();
 
-    if (isset($_GET['q']) && isset($_GET['show'])) {
-        $query = strtolower($_GET['q']);
-        $opt = $_GET['show'];
-    } else {
+    if (!isset($_GET['q']) || !isset($_GET['show'])) {        
         header("Location: index.html");
     }
 
-    $resultArray = $ListSearch->searchTextList($wordListFile, $query, $opt);
+    // Specify the search query (substring) value to look.
+    $query = strtolower($_GET['q']);
+    
+    // Specify the option for the results.
+    $option = $_GET['show'];
+
+    // Run the searchTextList function.
+    $resultArray = $ListSearch->searchTextList($wordListFile, $query, $option);
 ?>
 <html>
 <head>
@@ -22,6 +28,7 @@
     <div id="result-info">Found <?php echo count($resultArray); ?> results.</div>
     <div id="result-container">
 <?php
+    // Iterate through, and display/process the results.
     foreach ($resultArray as $result) {
         echo '<div class="item">'.str_replace($query, "<b>$query</b>", $result).'</div>';
     }
