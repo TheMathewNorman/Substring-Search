e7bff57a44b381d7532cc5a286a74b20b2b3e399
Mathew Norman <spam@matnorman.com>
2018-06-16 08:22:08 +1000

    Updated example
    Added some very basic styling.
    Generated a list of fake words.

--- example/results.php
+++ example/results.php
@@ -14,14 +14,15 @@ if (isset($_GET['q']) && isset($_GET['show'])) {
 <html>
 <head>
     <title>Results</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
 </head>
 <body>  
+    <div id="container">
 <?php
-    echo "<table>";
     foreach ($ListSearch->searchTextList($wordListFile, $query, $opt) as $result) {
-        echo $result;
+        echo '<div class="item">'.str_replace($query, "<b>$query</b>", $result).'</div>';
     }
-    echo "</table>";
 ?>
+    </div>
 </body>
 </html>
